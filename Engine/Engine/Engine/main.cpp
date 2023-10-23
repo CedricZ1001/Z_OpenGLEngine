83220dcb3b4ad7e477e350e7770535e2da811278
CedricZ1001 <Cedric1001Z@gmail.com>
2023-10-23 18:06:07 +0800

    update

--- Engine/Engine/Engine/main.cpp
+++ Engine/Engine/Engine/main.cpp
@@ -25,7 +25,7 @@ const char* fragmentShaderSource =
 "#version 330 core\n"
 "out vec4 FragColor;\n"
 "void main(){\n"
-"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);}\n";
+"FragColor = vec4(1.0f, 0.8f, 0.2f, 1.0f);}\n";
 
 
 
