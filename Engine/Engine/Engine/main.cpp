27cb0632c117ce0cc416374e2a8178dd3b4d914f
CedricZ1001 <Cedric1001Z@gmail.com>
2023-10-29 20:32:43 +0800

    update glm

--- Engine/Engine/Engine/main.cpp
+++ Engine/Engine/Engine/main.cpp
@@ -1,10 +1,13 @@
 ﻿#define GLEW_STATIC
 #define STB_IMAGE_IMPLEMENTATION
 #include<iostream>
-#include <GL/glew.h>
+#include<GL/glew.h>
 #include<GLFW/glfw3.h>
-#include "stb_image.h"
-#include "Shader.h"
+#include"stb_image.h"
+#include<glm.hpp>
+#include<gtc/matrix_transform.hpp>
+#include<gtc/type_ptr.hpp>
+#include"Shader.h"
 
 
 /*透视投影
@@ -13,9 +16,7 @@ f/aspect,	0,		0,			0,
 	0,		f,		0,			0,
 	0,		0,(f+n)/(n-f),	(2*f*n)/(n-f),
 	0，		0,		-1,			0
-}
-*/
-
+}*/
 
 //float vertices[] = {
 //	-0.5f, -0.5f, 0.0f,
@@ -26,7 +27,6 @@ f/aspect,	0,		0,			0,
 //	 0.9f,  0.5f,  0.0f
 //};
 
-
 //float vertices[] = {
 //	-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
 //	 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
@@ -36,6 +36,7 @@ f/aspect,	0,		0,			0,
 //	 0.9f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f,
 //	 -0.9f,  0.5f,  0.0f, 0.0f, 1.0f, 0.0f
 //};
+
 float vertices[] = {
 	//     ---- 位置 ----       ---- 颜色 ----     - 纹理坐标 -
 		 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // 右上
@@ -50,11 +51,11 @@ float vertices[] = {
 //	2, 1, 3,
 //	2, 4, 0
 //};
+
 unsigned int indices[] = {
 	0, 1, 2,
 	2, 3, 0
 };
-
 
 //const char* vertexShaderSource =
 //"#version 330 core\n"
@@ -74,7 +75,6 @@ unsigned int indices[] = {
 //"void main(){\n"
 //"FragColor = vertexColor;}\n";
 
-
 void processInput(GLFWwindow* window ) { //输入检测
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
 		glfwSetWindowShouldClose(window, true);
@@ -82,24 +82,28 @@ void processInput(GLFWwindow* window ) { //输入检测
 }
 
 int main() {
-	
+	//glm::vec4 vec(1.0f, 0.0f, 0.0f,1.0f);
+	//std::cout << vec.x << vec.y << vec.z << std::endl;
+	//glm::mat4 trans;
+	//trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+	//vec = trans * vec;
+	//std::cout << vec.x << vec.y << vec.z << vec.w;
+
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);//3.3开始使用可编程渲染管线
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);//早期固定流水线，现在可编程化流水线一般都使用GLFW_OPENGL_CORE_PROFILE，另一个配置GLFW_OPENGL_COMPAT_PROFILE可以使用过时的特性和固定流水线
 	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // on Mac OS X you need to add
 
-	GLFWwindow* window = glfwCreateWindow(1080,960,"MY OpenGL Game",NULL,NULL);
+	GLFWwindow* window = glfwCreateWindow(1080, 960, "MY OpenGL Game", NULL, NULL);
 	// Open GLFW window
-	if (window == NULL)
-	{
+	if (window == NULL) {
 		printf("failed");
 		glfwTerminate();
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
 	
-
 	//Init GLEW
 	glewExperimental = true; //实验性模式
 	if (glewInit() != GLEW_OK) {
@@ -109,12 +113,14 @@ int main() {
 	}
 
 	glViewport(0, 0, 1080, 960); //设置视口(Viewport)的大小 及定义屏幕上绘制图形的位置和大小
+
 	//glEnable(GL_CULL_FACE);
 	//glCullFace(GL_BACK);
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	//glEnable(GL_BLEND);//混合模式
 	//glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	//声明VBO 存储模型数据 
+	//声明VBO 存储模型数据
+
 	unsigned int VBO;
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -122,7 +128,7 @@ int main() {
 	//绑定VBO
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	//声明VAO 
+	//声明VAO
 	unsigned int VAO;
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
@@ -141,7 +147,6 @@ int main() {
 	glGenBuffers(1, &EBO);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-	
 
 	//init shader
 	Shader* myshader = new Shader("vertexSource.txt", "fragmentSource.txt");
@@ -178,41 +183,46 @@ int main() {
 	}
 	stbi_image_free(dataB);
 
-
-	//unsigned int vertexShader;
-	//vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	//glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	//glCompileShader(vertexShader);
-
-	//unsigned int fragmentShader;
-	//fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	//glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	//glCompileShader(fragmentShader);
-
-	//unsigned int shaderProgram;
-	//shaderProgram = glCreateProgram();
-	//glAttachShader(shaderProgram, vertexShader);
-	//glAttachShader(shaderProgram, fragmentShader);
-	//glLinkProgram(shaderProgram);
-
 	//获取事件 找到ourcolor的地址
 	/*float timeValue = glfwGetTime();
 	float greenValue = (sin(timeValue) / 2) + 0.5f;
 	int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");*/
 	
+	glm::mat4 trans;
+	//trans = glm::translate(trans, glm::vec3(1.0f, 0.0f, 0.0f));
+	//trans = glm::rotate(trans, glm::radians(45.0f), glm::vec3(0, 0, 1.0f));
+	//trans = glm::scale(trans, glm::vec3(2.0f, 2.0f, 2.0f));
+	//bool isbigger = true;s
 
 	while (!glfwWindowShouldClose(window)) {
-		
+		trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0, 0, 1.0f));
 		processInput(window);
-
+		//std::cout << trans[3].x << std::endl;
+		/*if (isbigger == false) {
+			trans = glm::scale(trans, glm::vec3(0.99f, 0.99f, 0.99f));
+			isbigger = trans[0].x < 1.0f ? 1 : 0;
+		}
+		else {
+			trans = glm::scale(trans, glm::vec3(1.01f, 1.01f, 1.01f));
+			isbigger = trans[0].x > 2.0f ? 0 : 1;
+		}*/
+		//trans = glm::translate(trans, glm::vec3(0.01f, 0.0f, 0.0f));
+		//trans = glm::rotate(trans, glm::radians(1.0f), glm::vec3(0, 0, 1.0f));
+		
 		glClearColor(0.2f, 0.3f, 0.5f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);//The possible bits we can set are GL_COLOR_BUFFER_BIT, GL_DEPTH_BUFFER_BIT and GL_STENCIL_BUFFER_BIT.
+		
 		//timeValue = glfwGetTime();
 		//greenValue = (sin(timeValue) / 2) + 0.5f;
-		glUniform1i(glGetUniformLocation(myshader->ID, "ourTexture"),0);
+
+		glUniform1i(glGetUniformLocation(myshader->ID, "ourTexture"), 0);
 		glUniform1i(glGetUniformLocation(myshader->ID, "leatherTexture"), 1);
+		//calculate our transform
+		unsigned int transformLoc = glGetUniformLocation(myshader->ID, "transform");
+		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 		myshader->Use();
 		//glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+		
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, TexBufferB);
 		glActiveTexture(GL_TEXTURE0);
